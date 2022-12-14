294dcefca8fb82c9aee10c582e666cba5991d402
Gabriel Freire <gabrielfvale@gmail.com>
2022-12-14 15:39:55 -0300

    feat: add models

--- amicci_briefing/api/models.py
+++ amicci_briefing/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Briefing(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, null=False)
+    retailer = models.CharField(max_length=200)
+    responsible = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    release_date = models.CharField(max_length=200)
+    available = models.PositiveSmallIntegerField()
+
+
+class Retailer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    retailer = ArrayField(models.IntegerField())
+
+
+class Vendor(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
