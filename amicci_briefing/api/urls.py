863420dbb1b5b195d6e57d2bf663edebf9b6688c
Gabriel Freire <gabrielfvale@gmail.com>
2022-12-15 03:46:09 -0300

    chore: add missing trailing slashes to retrieve urls

--- amicci_briefing/api/urls.py
+++ amicci_briefing/api/urls.py
@@ -7,19 +7,19 @@ from amicci_briefing.api import views
 urlpatterns = [
     # Briefing
     path("briefings/", views.BriefingListView.as_view()),
-    path("briefing/<int:pk>", views.BriefingRetrieveUpdateView.as_view()),
+    path("briefing/<int:pk>/", views.BriefingRetrieveUpdateView.as_view()),
     path("briefing/", views.BriefingCreateView.as_view()),
     # Retailer
     path("retailers/", views.RetailerListView.as_view()),
-    path("retailer/<int:pk>", views.RetailerRetrieveUpdateView.as_view()),
+    path("retailer/<int:pk>/", views.RetailerRetrieveUpdateView.as_view()),
     path("retailer/", views.RetailerCreateView.as_view()),
     # Vendor
     path("vendors/", views.VendorListView.as_view()),
-    path("vendor/<int:pk>", views.VendorRetrieveUpdateView.as_view()),
+    path("vendor/<int:pk>/", views.VendorRetrieveUpdateView.as_view()),
     path("vendor/", views.VendorCreateView.as_view()),
     # Category
     path("categories/", views.CategoryListView.as_view()),
-    path("category/<int:pk>", views.CategoryRetrieveUpdateView.as_view()),
+    path("category/<int:pk>/", views.CategoryRetrieveUpdateView.as_view()),
     path("category/", views.CategoryCreateView.as_view()),
     # Other
     path(
